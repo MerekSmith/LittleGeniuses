de60c147a0f63e396a3d7a45411deb9f81b4f981
Merek Smith <mereksmith@Hotmail.com>
2019-08-22 09:51:57 -0600

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://localhost/littlegeniuses",
-  secretOrKey: "SECRET"
-};
-
-// mongodb+srv://Merek:RPlRhNgp54X2i70P@little-geniuses-crhar.mongodb.net/test?retryWrites=true&w=majority
